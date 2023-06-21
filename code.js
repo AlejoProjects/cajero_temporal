2b6afc99107e1092a13fc2a7b62ff43314eaddd5
alejandro <alejodelavinci@gmail.com>
2023-06-20 19:33:48 -0500

    se añadio la documentación a la ultima función

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 let logButton = document.getElementById('log_in_button');
-let users = [{name:"joe",userName:"pal",password:"123"},{name:"pat",userName:"ghanima",password:"321"},{name:"aldro",userName:"a",password:"1"}];
+let users = [{ name: "joe", userName: "pal", password: "123" }, { name: "pat", userName: "ghanima", password: "321" }, { name: "aldro", userName: "a", password: "1" }];
 class cliente {
     /**La clase cliente tiene las variables de nombre, contraseña  y dinero.
      * Las funciones principales son las relacionadas a la manipulación del dinero.
@@ -7,7 +7,7 @@ class cliente {
      *password es de tipo string
      *money es de tipo double
     */
-    constructor(name,user, password, money) {
+    constructor(name, user, password, money) {
         name = this.name;
         user = this.user;
         password = this.password;
@@ -70,13 +70,13 @@ function removeUserLog() {
     userWindow = document.getElementById('user_log');
     userWindow.remove();
 }
-function testClient(use,pass){
+function testClient(use, pass) {
     let validate = false;
-    let respuesta = [validate,"","",""];
-    for(i in users){    
-        if(use == users[i].userName && pass == users[i].password){
+    let respuesta = [validate, "", "", ""];
+    for (i in users) {
+        if (use == users[i].userName && pass == users[i].password) {
             validate = true;
-            respuesta = [validate,users[i].name,users[i].userName,users[i].password];
+            respuesta = [validate, users[i].name, users[i].userName, users[i].password];
             break;
         }
     }
@@ -92,18 +92,18 @@ function defineClient() {
     let password = document.getElementById('password_input').value;
     username = username.toString();
     password = password.toString();
-    test = testClient(username,password);
+    test = testClient(username, password);
     bool = test[0];
-    if(bool){
+    if (bool) {
         client.names = test[1];
-        client.users= test[2];
+        client.users = test[2];
         client.passwords = test[3];
         client.moneys = 500000;
         removeUserLog();
-        mainMenu.style.display  = 'block';
-        mainMenuText.innerHTML = 'hola '+client.names + ' bienvenido al portal principal';
+        mainMenu.style.display = 'block';
+        mainMenuText.innerHTML = 'Hola ' + client.names + ' bienvenido al portal principal';
     }
-    else{
+    else {
         alert('datos incorrectos, intentelo de nuevo porfavor');
     }
 
@@ -112,22 +112,46 @@ function hideButtons() {
     let buttonMenu = document.getElementById("main_menu");
     buttonMenu.style.display = "none";
 }
-function addSaldoWindow() {
-    hideButtons();
+function addSaldoWindow(valueCase) {
+    /* La función addSaldoWindow crea una ventana donde dependiendo del boton pulsado se 
+     insertaran los elementos necesarios para:
+     ->añadir saldo
+     ->Retirar saldo
+     ->consultar saldo
+     */ 
+    /*se definen los elementos principales de la ventana*/ 
     const ventanaPrincipal = document.createElement("div");
     const textoPrueba = document.createElement("p");
     const textoSaldo = document.createElement("p");
+    const textoBoton = document.createElement("span");
+    const valorIngresado = document.createElement("input");
+     /**Boton transacción se refiere al boton que tomara la información del input donde se retirara o se añadira el saldo */
+    const botonTransaccion = document.createElement("button");
+    /** Se refiere al texto que contiene los elementos*/
     const node = document.createTextNode("Su saldo actual es de");
     const node2 = document.createTextNode(client.moneys);
+    const node3 = document.createTextNode("submit");
+    /*añade los textos a los parrafos y botones */ 
     textoPrueba.appendChild(node);
     textoSaldo.appendChild(node2);
+    botonTransaccion.appendChild(node3);
     ventanaPrincipal.className = "window";
+    valorIngresado.className = "inputValue";
+    /**IMPORTANTE Añadir id y utilizarlo en una función */
+    botonTransaccion.className = "boton_transaccion";
+    /**Finalmente añade los elementos */
     body = document.getElementById('main_window');
     body.appendChild(ventanaPrincipal);
+    /**Si el valor es igual a uno se añade el input al html */
+    if(valueCase == 1){
+        ventanaPrincipal.appendChild(valorIngresado);
+        ventanaPrincipal.appendChild(botonTransaccion);
+    }
     ventanaPrincipal.appendChild(textoPrueba);
     ventanaPrincipal.appendChild(textoSaldo);
-
+    /*esconde la ventana de los botones*/
+    hideButtons();
 }
-/**Lo que sigue simplemente es manipular los elementos del DOM para mostrar las ventanas según la opción que se pida. */
-/**La ventana de los botones solo se le hace display. */
-/**el resto de las ventanas se crean directamente */
+/**Lo que sigue es asignar una id a los elementos añadidos y crear dos funciones que utilicen las funciones addMoney(value) y substractMoney(value) de la clase client */
+/** Añadir la colección de transacciones*/
+/**Si queda tiempo utilizar bien el json*/
